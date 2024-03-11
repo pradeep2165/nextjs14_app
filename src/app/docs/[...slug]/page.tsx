14857ad50da5743455d9ca4bb4e315b59a03bf8a
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 08:02:13 +0530

    catch-all-route and params

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -1,8 +1,24 @@
-export default function docs() {
+export default function docs({
+  params,
+}: {
+  params: {
+    slug: string[];
+  };
+}) {
+  if (params.slug.length === 2) {
+    return (
+      <h1>
+        viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+      </h1>
+    );
+  } else if (params.slug.length === 1) {
+    return <h1>Viewing docs for feature {params.slug[0]}</h1>;
+  }
   return (
     <>
       <h1>Docs home page</h1>
       <p>http://localhost:3000/docs/feature1/concept1 and so on ...</p>
+      {/* this will will not show */}
     </>
   );
 }
