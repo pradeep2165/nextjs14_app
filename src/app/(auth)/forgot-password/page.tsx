1ddf97b2b3b798b9b365ae1a642ff4fb8d2ea3b7
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 08:44:31 +0530

    added in route group

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+export default function ForgotPassword() {
+  return <h1>Forgot Password</h1>;
+}
+
+//now auth foler in route group and its omited its name, hence can be accessed /forgot-password and so for same for all other
--- src/app/auth/forgot-password/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ForgotPassword() {
-  return <h1>Forgot Password</h1>;
-}
-
-//all router in one folder and this can access using /auth/forgot-password and so for same all other route
