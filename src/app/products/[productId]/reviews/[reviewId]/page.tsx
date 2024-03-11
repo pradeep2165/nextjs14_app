b8ccb8eac9244e7d056610a97ef77e4feb3b3746
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 08:14:28 +0530

    accessing not-found page in nested component

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({ params }: { params: { productId: string; reviewId: string } }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <>
       review {params.reviewId} for product {params.productId}{" "}
