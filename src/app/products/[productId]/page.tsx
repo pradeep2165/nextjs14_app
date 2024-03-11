ebc382bd3b92a4d5346a22c5afd5a299b159583e
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 16:00:25 +0530

    title added using metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,15 @@
-export default function productDetails({ params }: { params: { productId: string } }) {
+import { Metadata } from "next";
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+export default function productDetails({ params }: Props) {
   return (
     <>
       <h1>product details {params.productId}</h1>
