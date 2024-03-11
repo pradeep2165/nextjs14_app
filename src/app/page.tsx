976db071d07db77a5868382e0063d1334dd14164
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 16:43:46 +0530

    linking route is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
 export default function () {
-  return <h1>Hello wrold</h1>;
+  return (
+    <>
+      <h1>Hello wrold</h1>
+      <Link href="/products" className="mr-4 text-blue-400">
+        product
+      </Link>
+      <Link href="/blog" className="text-blue-400">
+        Blog
+      </Link>
+    </>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 export default function products() {
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>Product list</h1>
-      <h2>product1</h2>
-      <h2>product2</h2>
-      <h2>product3</h2>
+      <h2>
+        <Link href="products/1">product1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">product2</Link>
+      </h2>
+      <h2>
+        <Link href="products/3">product3</Link>
+      </h2>
     </>
   );
 }
