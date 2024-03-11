797cb233924a8398c80cf2922200ac7a2561775c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 07:37:06 +0530

    dynamic route

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,7 @@
+export default function productDetails() {
+  return (
+    <>
+      <h1>product details</h1>
+    </>
+  );
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function products() {
+  return (
+    <>
+      <h1>Product list</h1>
+      <h2>product1</h2>
+      <h2>product2</h2>
+      <h2>product3</h2>
+    </>
+  );
+}
