29d61536702ab45c72698a7c0731d7c1b13f3f6d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 08:04:32 +0530

    optional catch-all-route

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -5,13 +5,13 @@ export default function docs({
     slug: string[];
   };
 }) {
-  if (params.slug.length === 2) {
+  if (params.slug?.length === 2) {
     return (
       <h1>
         viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
       </h1>
     );
-  } else if (params.slug.length === 1) {
+  } else if (params.slug?.length === 1) {
     return <h1>Viewing docs for feature {params.slug[0]}</h1>;
   }
   return (
