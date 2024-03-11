c38f0dc4ba0a47ba216d9d6520fd6ba673f9a057
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 08:39:17 +0530

    grouped

---
+++ src/app/auth/forgot-password/page.tsx
@@ -0,0 +1,5 @@
+export default function ForgotPassword() {
+  return <h1>Forgot Password</h1>;
+}
+
+//all router in one folder and this can access using /auth/forgot-password and so for same all other route
