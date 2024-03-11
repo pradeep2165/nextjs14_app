6fd9a8ff35c17cded47f1ee39418c88d9901de37
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-11 21:54:25 +0530

    error-recovering

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,10 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <div>{error.message}</div>;
+export default function ErrorBoundary({ error, reset }: { error: Error; reset: () => void }) {
+  return (
+    <div>
+      <p>{error.message}</p>
+      <button onClick={reset}>try again</button>
+    </div>
+  );
 }
