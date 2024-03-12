bdb6f978bac120d87a8cd96de41246efa8fee873
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 17:19:12 +0530

    query handler

--- src/app/server_data/comments/route.ts
+++ src/app/server_data/comments/route.ts
@@ -1,6 +1,10 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query ? comments.filter((comment) => comment.text.includes(query)) : comments;
+  return Response.json(filteredComments);
 }
 //localhost:3000/server_data/comments
 
