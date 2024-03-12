379691d53644c4a80d5b051c4d00a4fc8d2f9cb9
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 18:13:04 +0530

    Headers in route handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,10 @@
-export async function GET(){
-    return new Response("Profile Data");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestHeaders = new Headers(request.headers);
+  const headersList = headers();
+  console.log(headersList.get("Authorization"));
+  console.log(requestHeaders.get("Authorization"));
+  return new Response("Profile Data");
 }
