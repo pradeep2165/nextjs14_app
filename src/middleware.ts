b041804c965dd4d2b57a34001de77aa911a9995c
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 20:10:46 +0530

    middleware with rewrite

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/hello", request.url));
+    return NextResponse.rewrite(new URL("/hello", request.url));
   }
   // return NextResponse.redirect(new URL("/", request.url));
 }
