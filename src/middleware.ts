7a96515e4b209e43f418d071d077e6fd6e3a1a00
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 20:09:33 +0530

    middleware to redirect to hello

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-  //   if (request.nextUrl.pathname === "/profile") {
-  //     return NextResponse.redirect(new URL("/hello", request.url));
-  //   }
-  return NextResponse.redirect(new URL("/", request.url));
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("/hello", request.url));
+  }
+  // return NextResponse.redirect(new URL("/", request.url));
 }
 
-export const config = {
-  matcher: "/profile",
-};
+// export const config = {
+//   matcher: "/profile",
+// };
