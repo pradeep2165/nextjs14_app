03924df74cba02f0d181e8b3cb01b47975a8a4f7
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 20:18:44 +0530

    cookie and headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.rewrite(new URL("/hello", request.url));
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
+  response.headers.set("customer-header", "custom-value");
+  return response;
+  // if (request.nextUrl.pathname === "/profile") {
+  //   return NextResponse.rewrite(new URL("/hello", request.url));
+  // }
   // return NextResponse.redirect(new URL("/", request.url));
 }
 
