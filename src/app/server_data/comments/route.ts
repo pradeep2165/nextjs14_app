a7682f631e972f1476f0161066934c7178218cc9
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 13:29:18 +0530

    post route handler

--- src/app/server_data/comments/route.ts
+++ src/app/server_data/comments/route.ts
@@ -3,3 +3,18 @@ export async function GET() {
   return Response.json(comments);
 }
 //localhost:3000/server_data/comments
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
