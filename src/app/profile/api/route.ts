c2230ec34b3c0e4dd1e1f9151b095ff553714770
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2024-03-12 18:57:55 +0530

    working with cookies

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,10 +1,19 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headersList = headers();
+  cookies().set("resultsPerPage", 20);
+  const theme = request.cookies.get("theme");
   console.log(headersList.get("Authorization"));
   console.log(requestHeaders.get("Authorization"));
-  return new Response("Profile Data");
+  console.log(theme);
+  console.log(cookies().get("resultsPerPage"));
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
+    },
+  });
 }
